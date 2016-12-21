88a738b90292945789db09c8d857e423305df090
Jirka Kremser <jkremser@redhat.com>
2016-12-21 16:23:16 +0100

    Making eslint happy

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 #!/usr/bin/env node
-'use strict';
 
-const chalk         = require('chalk');
-const clear         = require('clear');
-const CLI           = require('clui');
-const figlet        = require('figlet');
-const inquirer      = require('inquirer');
-const Preferences   = require('preferences');
-const Spinner       = CLI.Spinner;
-const _             = require('lodash');
-const fs            = require('fs');
-const wizzard       = require('./lib/wizzard');
-const dc            = require('./lib/dockerCompose');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const wizzard = require('./lib/wizzard');
 const checkDockerIsInstalled = require('./lib/checkCommands').checkDockerIsInstalled;
-const exec          = require('child_process').exec;
 
-
-// clear();
 console.log('\x1B[2J');
 console.log(
   chalk.blue(
