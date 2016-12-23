7adc6884bfc3801eb50bcc3e73b31e995eb92182
Jirka Kremser <jkremser@redhat.com>
2016-12-23 18:01:43 +0100

    Adding argument parsing and non-interactive mode.

--- index.js
+++ index.js
@@ -3,15 +3,55 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
 const wizzard = require('./lib/wizzard');
+const commandLineArgs = require('command-line-args')
 const checkDockerIsInstalled = require('./lib/checkCommands').checkDockerIsInstalled;
+const version = require('./package.json').version;
+const usage = require('./lib/usage');
+const optionDefinitions = require('./lib/options');
+const _ = require('lodash');
 
-console.log('\x1B[2J');
-console.log(
-  chalk.blue(
-    figlet.textSync('Hawkinit', {
-      horizontalLayout: 'fitted'
-    })
-  )
-);
 
-checkDockerIsInstalled(() => wizzard.show());
+const hawkinitWizzard = (save, timeout) => {
+  console.log('\x1B[2J');
+  process.stdout.write(
+    chalk.blue(
+      figlet.textSync('Hawkinit', {
+        horizontalLayout: 'fitted'
+      }).slice(0, -50)
+    )
+  );
+  console.log(chalk.grey(` (v${version})\n`));
+
+  checkDockerIsInstalled(() => wizzard.show(save, timeout));
+}
+
+const printVersion = () => console.log(version)
+
+const parseArgs = () => {
+  try {
+    const options = commandLineArgs(optionDefinitions);
+    if (_.isEmpty(options)) {
+      hawkinitWizzard();
+    } else if (options.version) {
+      printVersion();
+    } else if (options.help) {
+      usage.printUsage();
+    } else {
+      const save = options['save-answers'];
+      const timeout = !isNaN(options.timeout) ? options.timeout : 0;
+      const answerFile = options['answer-file'];
+      if (answerFile) {
+        console.log('running with answer file ' + answerFile.filename);
+        console.log('it ' + (answerFile.exists ? 'exists' : 'doesn\'t exist'));
+        // todo: run w/o the wizzard, but w/ timeout.. save option doesn't make sense here => ignore it
+      } else {
+        hawkinitWizzard(save, timeout);
+      }
+    }
+  } catch (err) {
+    console.log('\n' + chalk.red(err.message));
+    usage.printUsage();
+  }
+}
+
+parseArgs();
