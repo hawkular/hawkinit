28e80040fd0f865f51361ce651b03f43c1b28953
Jirka Kremser <jkremser@redhat.com>
2016-12-23 18:07:05 +0100

    eslint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
 const wizzard = require('./lib/wizzard');
-const commandLineArgs = require('command-line-args')
+const commandLineArgs = require('command-line-args');
 const checkDockerIsInstalled = require('./lib/checkCommands').checkDockerIsInstalled;
 const version = require('./package.json').version;
 const usage = require('./lib/usage');
@@ -23,9 +23,9 @@ const hawkinitWizzard = (save, timeout) => {
   console.log(chalk.grey(` (v${version})\n`));
 
   checkDockerIsInstalled(() => wizzard.show(save, timeout));
-}
+};
 
-const printVersion = () => console.log(version)
+const printVersion = () => console.log(version);
 
 const parseArgs = () => {
   try {
@@ -41,17 +41,17 @@ const parseArgs = () => {
       const timeout = !isNaN(options.timeout) ? options.timeout : 0;
       const answerFile = options['answer-file'];
       if (answerFile) {
-        console.log('running with answer file ' + answerFile.filename);
-        console.log('it ' + (answerFile.exists ? 'exists' : 'doesn\'t exist'));
+        console.log(`running with answer file ${answerFile.filename}`);
+        console.log(`it ${answerFile.exists ? 'exists' : 'doesn\'t exist'}`);
         // todo: run w/o the wizzard, but w/ timeout.. save option doesn't make sense here => ignore it
       } else {
         hawkinitWizzard(save, timeout);
       }
     }
   } catch (err) {
-    console.log('\n' + chalk.red(err.message));
+    console.log(`\n${chalk.red(err.message)}`);
     usage.printUsage();
   }
-}
+};
 
 parseArgs();
