2688e8c64e65992cfa65b1cac844711c17463cd0
Jirka Kremser <jkremser@redhat.com>
2017-05-12 23:13:20 +0200

    Adding more test permutations.

--- .travis.tests.sh
+++ .travis.tests.sh
@@ -1,20 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # simple tests that runs some basic scenarios of hawkinit
 # and tries to access the status endpoint
 
 set -xe
-
-# run linter
-npm run lint
-_return_code=$?
-# fail the test if linter fails
-[[ $_return_code != 0 ]] && exit 1
-
-# install hawkinit
-sudo npm install -g .
-_return_code=$?
-# fail the test if it's not possible to install hawkinit
-[[ $_return_code != 0 ]] && exit 1
 
 # generate the answer file
 cat >./answers <<EOL
@@ -34,16 +22,18 @@ cat >./answers <<EOL
 EOL
 
 # ssl
-if [[ "$TEST_SSL" = "1" ]] ; then
+if [[ "$SSL" = "1" ]] ; then
   echo '  "ssl": true,' >> ./answers
-  _hawkular_port="8443"
+  _port="8443"
+  _protocol="https"
 else
   echo '  "ssl": false,' >> ./answers
-  _hawkular_port="8080"
+  _port="8080"
+  _protocol="http"
 fi
 
 # domain vs standalone
-if [[ "$TEST_DOMAIN" = "1" ]] ; then
+if [[ "$DOMAIN" = "1" ]] ; then
   cat >>./answers <<EOL
   "wfType": "Domain",
   "domainScenario": "simple",
@@ -59,18 +49,24 @@ EOL
 fi
 
 # run hawkinit with the answer file and timeout 5 minutes
-echo "TEST_SSL=$TEST_SSL"
-echo "TEST_DOMAIN=$TEST_DOMAIN"
+echo "SSL=$SSL"
+echo "DOMAIN=$DOMAIN"
 echo "running hawkinit with answers: "
 cat ./answers
-hawkinit -a ./answers -t 5 &> /tmp/hawkinit.log &
+node ./index.js -a ./answers -t 5 &
+_return_code=$?
+# fail the test if it wasn't possible to run hawkinit
+[[ $_return_code != 0 ]] && exit 1
 
-# after 280 seconds try to curl the status endpoint
+
+# after 280 seconds try to do two http requests to the status endpoint
 echo "waiting 280 seconds for hawkinit to start."
+date
 sleep 280
-_http_code=`curl -k -s -I -o /dev/null -w "%{http_code}" http://localhost:$_hawkular_port/hawkular/status`
-[[ "$_http_code" -lt "200" || "$_http_code" -gt "299" ]] && echo "something went wrong, here is the log: " && \\
-  cat /tmp/hawkinit.log && exit 1
+curl -k -I $_protocol://localhost:$_port/hawkular/status
+_http_code=`curl -k -s -I -o /dev/null -w "%{http_code}" $_protocol://localhost:$_port/hawkular/status`
+echo "_http_code=$_http_code"
+[[ "$_http_code" -lt "200" || "$_http_code" -gt "299" ]] && exit 1
 
 # success
 echo "Test completed successfully!"
