6230472262ede58050c68d9ec20c5fec3c6a6270
Josejulio Martínez <jmartine@redhat.com>
2017-05-26 14:05:11 -0500

    Adds 'chai' to use expect-style syntax for testing.

--- test/unit/test-DockerComposeBuilder.js
+++ test/unit/test-DockerComposeBuilder.js
@@ -1,29 +1,33 @@
-const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+const inflector = require('inflected');
 
 const DockerComposeBuilder = require('../../lib/dockerComposeBuilder.js');
 
 describe('DockerComposeBuilder', function () {
   describe('Constructor', function () {
     it('should work', function () {
-      assert.doesNotThrow(() => new DockerComposeBuilder());
+      expect(() => new DockerComposeBuilder()).to.not.throw();
     });
     it('should work by passing a template', function () {
       const template = {
         version: '3'
       };
-      assert.doesNotThrow(() => new DockerComposeBuilder(template));
+      expect(() => new DockerComposeBuilder(template)).to.not.throw();
     });
+
     it('should work if version is an integer', function () {
       const template = {
         version: 3
       };
-      assert.doesNotThrow(() => new DockerComposeBuilder(template));
+      expect(() => new DockerComposeBuilder(template)).to.not.throw();
     });
+
     it('should fail if passing a template with not supported version', function () {
       const template = {
         version: 1
       };
-      assert.throws(() => new DockerComposeBuilder(template), Error);
+      expect(() => new DockerComposeBuilder(template)).to.throw(Error);
     });
   });
 
@@ -31,7 +35,7 @@ describe('DockerComposeBuilder', function () {
     it('should add a service', function () {
       const builder = new DockerComposeBuilder();
       builder.service('service');
-      assert(builder.hasService('service'));
+      expect(builder.hasService('service')).to.equal(true);
     });
 
     describe('Parameters', function () {
@@ -43,10 +47,53 @@ describe('DockerComposeBuilder', function () {
         service = builder.service('service');
       });
 
-      it('should accept one image', function () {
-        const imageName = 'image_name';
-        service.image = imageName;
-        assert(service.image === imageName);
+      describe('Image', function () {
+        it('should set', function () {
+          const imageName = 'image_name';
+          service.image = imageName;
+          expect(service.image).to.equal(imageName);
+        });
+      });
+
+      const tests = [
+        { context: 'ports', data: ['8090:8090', '8080:8080'], notFound: ['9090:9090'] },
+        { context: 'links', data: ['myCassandra', 'hawkular'], notFound: ['metrics'] },
+        { context: 'volumes', data: ['/tmp/opt/hawkular:/opt/data'], notFound: ['/notfound'] },
+        { context: 'environment', data: ['HAWKULAR_BACKEND=remote', 'CASSANDRA_NODES=myCassandra'], notFound: ['HAWKULAR_USE_SSL=1'] }
+      ];
+      tests.forEach(function (test) {
+        describe(`${inflector.capitalize(inflector.pluralize(test.context))}`, function () {
+          it('should be able to add', function () {
+            test.data.forEach(function (data) {
+              service[`add${inflector.classify(test.context)}`](data);
+            });
+            test.data.forEach(function (data) {
+              expect(service[`has${inflector.classify(test.context)}`](data)).to.equal(true);
+            });
+            test.notFound.forEach(function (notFound) {
+              expect(service[`has${inflector.classify(test.context)}`](notFound)).to.equal(false);
+            });
+          });
+
+          it('should be able to retrieve all', function () {
+            test.data.forEach(function (data) {
+              service[`add${inflector.classify(test.context)}`](data);
+            });
+            expect(service[tests.context]).to.equal(tests.data);
+          });
+
+          it('should be able to remove', function () {
+            test.data.forEach(function (data) {
+              service[`add${inflector.classify(test.context)}`](data);
+            });
+            test.data.forEach(function (data) {
+              expect(service[`remove${inflector.classify(test.context)}`](data)).to.equal(true);
+            });
+            test.data.forEach(function (data) {
+              expect(service[`has${inflector.classify(test.context)}`](data)).to.equal(false);
+            });
+          });
+        });
       });
     });
   });
